8dfa49a7181d8c347d0151dc1e1c22721fef1d24
KVehse <kean.vehse@gmail.com>
2023-02-06 15:08:34 +0100

    add nav bar

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Hello } from './Hello';
 import { Info } from './Info';
+import AppNavBar from "/imports/navBar/AppNavBar";
 
 export const App = () => (
   <div>
+      <AppNavBar/>
     <h1>Welcome to Meteor! test</h1>
     <Hello />
     <Info />
