b49bfea48030b425b0afea9201303fc1dc0a6fa6
Matthias Kammerl <matthias.kammerl@outlook.com>
2023-02-09 16:57:19 +0100

    fix?

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+D
+imports/ui/Hello.tsx,8\2\828260afab2fd91f96bf1d84df0198f02f14f00a
+K
+imports/ui/SchipselCard.tsx,3\5\3588cfe012a7993c8d60af81a308ac67fbc0051e
--- server/main.ts
+++ server/main.ts
@@ -1,16 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
 import {SchnipselCollection} from "/imports/api/schnipsel";
-import {SchnipselCard} from "/imports/ui/SchipselCard";
 
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
+// noinspection JSUnusedLocalSymbols
+const schnipselTask = () => SchnipselCollection.insert({createdBy: "someone again", text: "", title: ""});
 
-export async function insertSchnipsel({ title, text, createdBy }) {
-  await SchnipselCollection.insertAsync({ title, text, createdBy, createdAt: new Date() });
-}
-const schnipselTask = schnipsel => SchnipselCollection.insert({createdBy: "someone again", text: "", title: ""});
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
 });
